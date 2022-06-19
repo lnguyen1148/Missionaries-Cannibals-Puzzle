6526bfcf741e819b55f2b4d73747b10552de0f33
matthewiversen <iversenmatt@yahoo.com>
2022-06-19 03:20:29 -0700

    actions done

--- misscannibals.py
+++ misscannibals.py
@@ -7,6 +7,13 @@ class MissCannibals(Problem):
         self.C = C
         super().__init__(initial, goal)
 
+
+    def goal_test(self, state):
+        """Return bool of if state is the goal state"""
+
+        return state == self.goal
+
+
     def result (self, state, action):
         """ Given state, make a new state with the given action.
             Action assumed to be valid."""
@@ -39,14 +46,10 @@ class MissCannibals(Problem):
         m_right = self.M - m_left
         c_right = self.C - c_left
 
-        print("ml cl mr cr")
-        print(m_left, c_left, m_right, c_right, sep="  ")
+        # for testing
+        # print("ml cl mr cr onLeft")
+        # print(m_left, c_left, m_right, c_right, state[2], sep="  ")
 
-        # check for invalid starting state
-        # if c_left > m_left or c_right > m_right:
-        #     valid_actions.append("invalid state")
-        # else: 
-            # boat is on the left side
         if state[2]:
             if m_left >= c_left + 2 and m_left >= 2:
                 valid_actions.append("MM")
@@ -56,6 +59,8 @@ class MissCannibals(Problem):
                 valid_actions.append("CC")
             if (m_right >= c_right + 1 or m_right == 0) and c_left >= 1:
                 valid_actions.append("C")
+            if (m_left >= 1 and c_left >= 1 and m_right >= c_right):
+                valid_actions.append("MC")
         # boat is on the right side
         else:
             if (m_right >= c_right + 2 or m_right == 2) and m_right >= 2:
@@ -66,28 +71,26 @@ class MissCannibals(Problem):
                 valid_actions.append("CC")
             if (m_left >= c_left + 1 or m_left == 0) and c_right >= 1:
                 valid_actions.append("C")
+            if (m_right >= 1 and c_right >= 1 and m_left >= c_left):
+                valid_actions.append("MC")
             
+        return valid_actions
 
-        return (valid_actions)
 
 if __name__ == '__main__':
     mc = MissCannibals(M=3, C=3)
-    # m, c, onLeft
-    test_state = (5, 6, True)
-    test_action = "MC"
-
-    print()
-    print( "Test state:", mc.goal_test(test_state) ) 
-    print( "'", test_action, "' : ", mc.result(test_state, test_action), sep="")
-    print()
     
     print(mc.actions((3, 2, True)))  # ['CC', 'C', 'M']
     print()
-    print(mc.actions((2, 2, False))) # ['M']
+    print(mc.actions((2, 2, False))) # ['M', 'MC']
     print()
-    print(mc.actions((1, 1, False))) # ['MM']
+    print(mc.actions((1, 1, False))) # ['MM', 'MC']
+    print()
+    print(mc.actions((1, 1, False))) # ['MM', 'MC']
+    print()
+    print(mc.actions((0, 2, True)))  # ['CC', 'C']
 
-    # path = depth_first_graph_search(mc).solution()
-    # print(path)
-    # path = breadth_first_graph_search(mc).solution()
-    # print(path)
+    path = depth_first_graph_search(mc).solution()
+    print(path)
+    path = breadth_first_graph_search(mc).solution()
+    print(path)
