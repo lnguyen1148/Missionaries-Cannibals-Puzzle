d6425ccb8098b1f769e556edca2a4c2bfba9c716
Linh Nguyen <lnguyen1148@csu.fullerton.edu>
2022-06-19 16:04:28 -0700

    Fix some tests

--- misscannibals.py
+++ misscannibals.py
@@ -7,60 +7,103 @@ class MissCannibals(Problem):
         self.C = C
         super().__init__(initial, goal)
 
+
     def goal_test(self, state):
+        """Return bool of if state is the goal state"""
+
         return state == self.goal
-    
-    def result(self, state, action):
+
+
+    def result (self, state, action):
+        """ Given state, make a new state with the given action.
+            Action assumed to be valid."""
+
         new_state = list(state)
 
-        if new_state[2] == True:
-            new_state[0] -= action.count('M')
-            new_state[1] -= action.count('C')
-            new_state[2] = False
-        else:
-            new_state[0] += action.count('M')
-            new_state[1] += action.count('C')
-            new_state[2] = True
-        
-        return tuple(new_state)
-    
-    def actions(self, state):
-        possible_actions = []
+        for letter in action:
+            # boat is on the left side
+            if new_state[2]:
+                if letter == "M":
+                    new_state[0] -= 1
+                if letter == "C":
+                    new_state[1] -= 1
+            # boat is on the right side
+            else:
+                if letter == "M":
+                    new_state[0] += 1
+                if letter == "C":
+                    new_state[1] += 1
 
+        # move the boat
+        new_state[2] = not new_state[2]
+            
+        return tuple(new_state)
+
+
+    def actions(self, state):
+        """Given state, return a list of all valid actions."""
+
+        valid_actions = []
         m_left = state[0]
         c_left = state[1]
         m_right = self.M - m_left
         c_right = self.C - c_left
 
-        if state[2] == True:                
-            if m_left > c_left and (m_right != 0 or c_right == 1):
-                possible_actions.append('M')
-            if m_left > c_left + 1 or m_left == 2:
-                possible_actions.append('MM')
-            if m_left == c_left and m_left > 0:
-                possible_actions.append('MC')
-            if c_left > 0 and (c_right < m_right or m_right == 0):
-                possible_actions.append('C')
-            if c_left > 1 and (c_right < m_right or m_right == 0):
-                possible_actions.append('CC')
+        # boat is on the left
+        if state[2]:
+            if (m_left >= c_left + 2 and m_left >= 2) or m_left == 2:
+                valid_actions.append("MM")
+            if (m_left >= c_left + 1 and m_left >= 1) or m_left == 1:
+                valid_actions.append("M")
+            if (m_right >= c_right + 2 or m_right == 0) and c_left >= 2:
+                valid_actions.append("CC")
+            if (m_right >= c_right + 1 or m_right == 0) and c_left >= 1:
+                valid_actions.append("C")
+            if (m_left >= 1 and c_left >= 1 and m_right >= c_right):
+                valid_actions.append("MC")
+        # boat is on the right side
+        else:
+            if (m_right >= c_right + 2 or m_right == 2) and m_right >= 2:
+                valid_actions.append("MM")
+            if (m_right >= c_right + 1 or m_right == 1) and m_right >= 1:
+                valid_actions.append("M")
+            if (m_left >= c_left + 2 or m_left == 0) and c_right >= 2:
+                valid_actions.append("CC")
+            if (m_left >= c_left + 1 or m_left == 0) and c_right >= 1:
+                valid_actions.append("C")
+            if (m_right >= 1 and c_right >= 1 and m_left >= c_left):
+                valid_actions.append("MC")
+            
+        return valid_actions
 
-        else:                
-            if m_right > c_right and (m_left != 0 or c_left == 1):
-                possible_actions.append('M')
-            if m_right > c_right + 1 or m_right == 2:
-                possible_actions.append('MM')
-            if m_right == c_right and m_right > 0:
-                possible_actions.append('MC')
-            if c_right > 0 and (c_left < m_left or m_left == 0):
-                possible_actions.append('C')
-            if c_right > 1 and (c_left < m_left or m_left == 0):
-                possible_actions.append('CC')
-
-        return possible_actions
 
 if __name__ == '__main__':
     mc = MissCannibals(M=3, C=3)
-    print(mc.actions((3, 2, True))) # Test your code as you develop! This should return  ['CC', 'C', 'M']
+    
+    print(mc.actions((3, 3, True)))  # ['sCC','C','MC'] - correct
+    print()
+    print(mc.actions((3, 3, False))) # [] - correct
+    print()
+    print(mc.actions((3, 2, True)))  # ['CC','C','M']  - correct
+    print()
+    print(mc.actions((3, 2, False))) # ['C'] - correct
+    print()
+    print(mc.actions((2, 2, True))) # ['MC', 'MM'] - correct
+    print()
+    print(mc.actions((2, 2, False))) # ['MC', 'M'] - correct 
+    print()
+    print(mc.actions((1, 1, True))) # ['MC', 'M'] - correct
+    print()
+    print(mc.actions((1, 1, False))) # ['MM', 'MC'] - correct
+    print("Linh removed invalid states and added more valid states to test")
+    print(mc.actions((3, 1, True))) # ['C', 'MM'] - incorrect
+    print()
+    print(mc.actions((3, 1, False))) # ['CC', 'C'] - correct
+    print()
+    print(mc.actions((3, 0, True))) # [] - incorrect
+    print()
+    print(mc.actions((3, 0, False))) # ['CC', 'C'] - correct
+    print()
 
     path = depth_first_graph_search(mc).solution()
     print(path)
